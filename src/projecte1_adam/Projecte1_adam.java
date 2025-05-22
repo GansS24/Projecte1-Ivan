7a92e9f0388d89f085140e3f397df1074f3fc640
GansS24 <ivanm95.29@gmail.com>
2025-05-22 17:34:31 +0200

    Yo que see brother

--- src/projecte1_adam/Projecte1_adam.java
+++ src/projecte1_adam/Projecte1_adam.java
@@ -5,6 +5,8 @@ public class Projecte1_adam {
     public static void main(String[] args) {
         
         System.out.println("Hola");
+        System.out.println("adeu");
+        System.out.println("Yo ke see brother que quieres que te diga.");
     }
     
 }
